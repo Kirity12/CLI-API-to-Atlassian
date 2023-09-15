582e397fbf78447dc74b53efc1a1d5e66383f476
Kirity <kiritykumar1998@gmail.com>
2023-09-15 11:39:10 -0700

    Added Labels to cards

--- trello_api.py
+++ trello_api.py
@@ -9,120 +9,110 @@ SECRET_KEY = "aa7a7b4c1cd435eb43df7175608ebd531b9295ef79a0428f484959584f67d980"
 
 class TrelloApi(object):
     def __init__(self, apikey, token=None):
-        self._apikey = API_KEY
-        self._token = TOKEN_KEY
+        self._apikey = apikey
+        self._token = token
         self.headers = {
                         "Accept": "application/json"
                        }
+        self.query = {
+                'key': self._apikey,
+                'token': self._token,
+                }
         self.url = "https://api.trello.com/1/"
 
     def raise_or_json(self, resp):
-        return json.dumps(json.loads(resp.text), indent=4, separators=(",", ": "))
+        status = resp.status_code
+        if status==200:
+            obj = json.dumps(json.loads(resp.text), indent=4, separators=(",", ": "))
+        else:
+            obj = None
+        return status, obj
 
     def set_token(self, token):
         self._token = token
         self.cards._token = token
 
-    def new_card(self, name, idList, desc=None, pos=None, due=None, start=None, dueComplete=None, idMembers=None, idLabels=None, urlSource=None, fileSource=None, idCardSource=None, keepFromSource=None):
+    def add_card(self, name, boardId, desc=None, idLabels=None):
         
         url = self.url + "cards"
         query = {
-                'key': API_KEY,
-                'token': TOKEN_KEY,
+                'key': self._apikey,
+                'token': self._token,
                 'name': name,
                 'desc': desc,
-                'idList': idList
+                'idList': boardId,
+                'idLabels': ['6503765a59075ce50db4ac43','6503765a59075ce50db4ac47']
                 }
         
         response = requests.request(
                                     "POST",
                                     url,
                                     params=query,
-                                    headers=headers
+                                    headers=self.headers,
                                     )
 
         return self.raise_or_json(response)
-        
     
     def get_boards(self):
-        url = self.url + "boards"
-        query = {
-                'key': self._apikey,
-                'token': self._token
-                }
+        url = self.url + "/members/me/boards"
+
         response = requests.request(
                                     "GET",
                                     url,
-                                    params=query,
-                                    headers=headers
+                                    params=self.query,
+                                    headers=self.headers
                                     )
 
         return self.raise_or_json(response)
     
     def get_columns(self, board_id):
-
         url = self.url + f"boards/{board_id}/lists"
-        query = {
-                'key': self._apikey,
-                'token': self._token,
-                'id': board_id
-                }
+
         response = requests.request(
                                     "GET",
                                     url,
-                                    params=query,
-                                    headers=headers
+                                    params=self.query,
+                                    headers=self.headers
+                                    )
+
+        return self.raise_or_json(response)
+    
+    def get_labels(self, board_id):
+
+        url = self.url + f"boards/{board_id}/labels"
+
+        response = requests.request(
+                                    "GET",
+                                    url,
+                                    params=self.query,
+                                    headers=self.headers
                                     )
 
         return self.raise_or_json(response)
     
     def get_token_url(self, app_name, expires='30days', write_access=True):
-        return f'''https://trello.com/1/authorize?key={self._apikey}& 
-                                                name={quote(app_name)}&
-                                                expiration={expires}&
-                                                response_type=token&
-                                                scope={'read,write' if write_access else 'read'}'''
-    
-    def set_token(self, token):
-        self._token = token
-
-
-# url = "https://api.trello.com/1/boards/6503765a9dfc3faf1dc65d24/lists"
-# url = "https://api.trello.com/1/members/me/boards"
-
-def get_token_url( app_name, expires='30days', write_access=True):
         url = f"https://trello.com/1/authorize?key={API_KEY}&"
         url+= f"name={quote(app_name)}&"
         url+= f"expiration={expires}&"
         url+= f"response_type=token&"
         url+= f"scope={'read,write' if write_access else 'read'}"
         return url
+    
+    def set_token(self, token):
+        self._token = token
 
-print(get_token_url('NG'))
+board_id = "6503765a9dfc3faf1dc65d24"
+col_id = "6503765a250478954fb2c0f2"
 
-url = "https://api.trello.com/1/cards"
+app = TrelloApi(API_KEY,TOKEN_KEY)
 
-headers = {
-  "Accept": "application/json"
-}
-
-query = {
-#   'id': '6503765a9dfc3faf1dc65d24',
-  'key': API_KEY,
-  'token': TOKEN_KEY,
-  'name': "Test Card",
-  'desc': "Doing",
-  'idList': "6503765a250478954fb2c0f2"
-}
-
-# response = requests.post(url, json=query)
-# print(json.loads(response.text))
-
-response = requests.request(
-   "POST",
-   url,
-   headers=headers,
-   params=query
-)
-
-print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+st, _ = app.get_boards()
+print(st)
+st, _ = app.get_columns(board_id)
+print(st)
+st, obj = app.get_labels(board_id)
+print(st)
+st = app.get_token_url('NG')
+print(st)
+st, _ = app.add_card('TEST', col_id, 'test')
+print(st)
